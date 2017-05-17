71b3ec2ff3e6fee44ce934bc9b0dfd9b8370c692
Rogelio Segovia <segovro@gmail.com>
2017-05-17 11:36:08 +0200

    Update ricardianToken.js

--- ricardianToken.js
+++ ricardianToken.js
@@ -1,3 +1,21 @@
-/**
- * 
- */
+if (typeof web3 !== 'undefined')
+	web3 = new Web3(web3.currentProvider);
+else
+	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+var RicardianTokenContract = web3.eth.contract();
+
+var RTC = RicardianTokenContract
+		.at("");
+
+var accounts = web3.eth.accounts;
+var nrAcc = accounts.length;
+
+web3.eth.defaultAccount = accounts[0];
+var myCoinbase = web3.eth.defaultAccount;
+
+var name = RTC.name;
+var symbol = RTC.symbol;
+var decimals = RTC.decimals;
+var totalSupply = RTC.totalSupply;
+var owner = RTC.owner;
